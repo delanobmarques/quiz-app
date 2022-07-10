5283253c127ea922c86916c246b6d57484dad1c1
delanobmarques <delanobmarques@gmail.com>
2022-07-10 13:46:23 -0300

    add axios, fetch data from api, make data available to settings component

--- src/components/select-field.component.jsx
+++ src/components/select-field.component.jsx
@@ -2,19 +2,23 @@ import React, { useState } from 'react';
 import { Box, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
 
 const SelectField = (props) => {
-    const {label} = props;
+    const {label, options} = props;
     const [value, setValue] = useState('');
 
-    const handleChange = () => {}
+    const handleChange = (event) => {
+      setValue(event.target.value)
+    }
 
   return (
     <Box mt={3} width="100%">
         <FormControl size="small" fullWidth>
             <InputLabel>{label}</InputLabel>
-            <Select value={value} label={label} onChage={handleChange}>
-                <MenuItem>option1</MenuItem>
-                <MenuItem>option2</MenuItem>
-                <MenuItem>option3</MenuItem>
+            <Select value={value} label={label} onChange={handleChange}>
+                {options.map(({id,name}) => (
+                  <MenuItem value={id} key={id }>
+                    {name}
+                  </MenuItem>
+                ))}                
             </Select>
         </FormControl>
     </Box>
--- src/pages/settings.page.jsx
+++ src/pages/settings.page.jsx
@@ -1,8 +1,38 @@
-import { Typography, Box, Button } from "@mui/material";
+import { Typography, Box, Button , CircularProgress } from "@mui/material";
+import useAxios from "../api/useAxios";
 import SelectField from "../components/select-field.component";
 import TextFieldComp from "../components/text-field.component";
 
 const Settings = () => {
+    const { response, error, loading } = useAxios({ url: "/api_category.php" });
+
+    if(loading){
+        return (
+            <Box mt={20}>
+                <CircularProgress/>
+            </Box>
+        )
+    }
+
+    if(error) {
+        return (
+            <Typography variant="h5" mt={20} color="red">
+                Sorry! Something went wrong.
+            </Typography>
+        )
+    }
+
+    const difficultyOptions = [
+        {id: "easy", name: "Easy"},
+        {id: "medium", name: "Medium"},
+        {id: "hard ", name: "Hard"},
+    ]
+
+    const typeOptions = [
+        {id: "multiple", name: "Multiple Choice"},
+        {id: "boolean", name: "True or False"},
+    ]
+
     const handleSubmit = (e) => {
         e.preventDefault();
     };
@@ -13,9 +43,9 @@ const Settings = () => {
                 Quiz App
             </Typography>    
             <form onSubmit={handleSubmit}>
-                <SelectField label="Category"/>
-                <SelectField label="Difficulty"/>
-                <SelectField label="Type"/>
+                <SelectField options={response.trivia_categories} label="Category"/>
+                <SelectField options={difficultyOptions} label="Difficulty"/>
+                <SelectField options={typeOptions} label="Type"/>
                 <TextFieldComp/>
                 <Box mt={3} width="100%">
                     <Button fullWidth variant="contained" type="submit">
