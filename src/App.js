5400a8cc1a27188dab05b24431991d6a48edb9a7
delanobmarques <delanobmarques@gmail.com>
2022-07-10 19:53:13 -0300

    remove unnecessary home component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { Container } from "@mui/material";
 import { Box } from "@mui/system";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import FinalScreen from "./pages/final-screen.page";
-import Home from "./pages/home.page";
 import Questions from "./pages/questions.page";
 import Settings from "./pages/settings.page";
 
@@ -12,7 +11,6 @@ const App = () => {
       <Container maxWidth="sm">
         <Box textAlign="center" mt={5}>
           <Routes>
-            {/* <Route exact path="/" element={<Home/>}/> */}
             <Route exact path="/" element={<Settings/>}/>
             <Route exact path="/questions" element={<Questions/>}/>            
             <Route exact path="/score" element={<FinalScreen/>}/>
